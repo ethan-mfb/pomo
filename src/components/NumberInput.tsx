1a03a69df2bdb9efa5a1101d84c4620657bb124c
Azure DevOps Pipeline <pipeline@mfbtech.com>
2025-09-06 03:15:27 +0000

    init number input

---
+++ src/components/NumberInput.tsx
@@ -0,0 +1,27 @@
+export type NumberInputProps = {
+  id: string;
+  label: string;
+  value: number;
+  onChange: (value: number) => void;
+  min?: number;
+  max?: number;
+  placeholder?: string | number;
+};
+
+export function NumberInput(args: NumberInputProps) {
+  return (
+    <div className="number-input">
+      <label htmlFor={args.id}>{args.label}</label>{' '}
+      <input
+        id={args.id}
+        name={args.id}
+        type="number"
+        value={args.value}
+        onChange={(e) => args.onChange(Number(e.target.value))}
+        min={args.min}
+        max={args.max}
+        placeholder={args.placeholder?.toString()}
+      />
+    </div>
+  );
+}
