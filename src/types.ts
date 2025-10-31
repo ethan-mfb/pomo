615f5755a77b5b96303e246c87e70da3934e8786
Ethan Lemke <ethan@mfbtech.com>
2025-10-31 15:20:14 +0000

    Add SessionState interface to define work/break session parameters

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,13 @@
 import { THEMES } from './constants.ts';
 
 export type Theme = (typeof THEMES)[keyof typeof THEMES];
+
+// Represents parameters needed to start a work/break session.
+export type SessionState = {
+  /** Total duration (seconds) of the session. */
+  durationSeconds: number;
+  /** Epoch millis when the session should end. */
+  endTimestamp: number;
+  /** True if this is a break session rather than work. */
+  isBreak: boolean;
+};
